c776961dc20b0c6c46e820b6edac2c88f38b414d
Rasmus Bertelsen <rasmusmbertelsen@gmail.com>
2026-02-22 12:16:22 +0100

    Initial commit

---
+++ AdsTestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using AdsWrapper;
+
+var localIp = "192.168.1.43";
+
+var remoteIp = "192.168.1.10";
+var remoteName = "C6015";
+var remoteUser = "Administrator";
+var remotePassword = "1";
+ushort remotePort = 851;
+
+var ads = new AdsHandler(localIp);
+
+ads.AddRemoteRoute(remoteIp, remoteName, remoteUser, remotePassword);
+
+ads.SetupRoute(remotePort);
+
+ads.PrintState();
